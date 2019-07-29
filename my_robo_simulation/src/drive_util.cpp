db5359791e72a34111da054fe3387d5908916c37
sskitajima <s.kitajima0162@gmail.com>
2019-07-29 18:03:34 +0900

    d_Uの計算を正しく行えた。Dなしでの実験が可能

--- my_robo_simulation/src/drive_util.cpp
+++ my_robo_simulation/src/drive_util.cpp
@@ -83,14 +83,14 @@ visualization_msgs::MarkerArray my_robo::make_traj_marker_array()
   int k=0;
   float green=0;
   float red=0;
-// 候補の数/4だけループ
-  for (int i=0; i<DWA.PredictTraj.size();i ++){
+// 候補の数ループ
+  for (int i=0; i<DWA.PredictTraj.size();i +=3){
     //ROS_INFO("start put marker.");
 
     // float GREEN= (double)rand()/RAND_MAX;
 
     //予測時刻の数だけループ
-    for (int j = 0; j < DWA.PredictTraj[i].size(); j++)
+    for (int j = 0; j < DWA.PredictTraj[i].size(); j +=2)
     {
  
        //ROS_INFO("start loop.");
@@ -99,7 +99,7 @@ visualization_msgs::MarkerArray my_robo::make_traj_marker_array()
       marker_array.markers[k].header.stamp = ros::Time::now();
       marker_array.markers[k].ns = "cmd_vel_display";
       marker_array.markers[k].id = k;
-      marker_array.markers[k].lifetime = (ros::Duration)DWA.looprate;
+      marker_array.markers[k].lifetime = (ros::Duration)(1 /DWA.looprate)*2;   //2ループ存在
 
       // marker_array.markers[j].type = visualization_msgs::Marker::CUBE;
       marker_array.markers[k].type = visualization_msgs::Marker::SPHERE;
@@ -109,15 +109,15 @@ visualization_msgs::MarkerArray my_robo::make_traj_marker_array()
       marker_array.markers[k].scale.z = 0.05;
       marker_array.markers[k].pose.position.x = DWA.PredictTraj[i][j][1];
       marker_array.markers[k].pose.position.y = DWA.PredictTraj[i][j][2];
-      marker_array.markers[k].pose.position.z = 0;
+      marker_array.markers[k].pose.position.z =  0;
       marker_array.markers[k].pose.orientation.x = 0;
       marker_array.markers[k].pose.orientation.y = 0;
       marker_array.markers[k].pose.orientation.z = 0;
       marker_array.markers[k].pose.orientation.w = 1;
 
-      marker_array.markers[k].color.r = 0.0f;
-      marker_array.markers[k].color.g = 1.0f;
-      marker_array.markers[k].color.b = 0.0f;
+      marker_array.markers[k].color.r = 1.0f;
+      marker_array.markers[k].color.g = DWA.CandVel[i][2];
+      marker_array.markers[k].color.b = DWA.CandVel[i][2];
       marker_array.markers[k].color.a = 1.0f;
       k++;
 
@@ -129,7 +129,7 @@ visualization_msgs::MarkerArray my_robo::make_traj_marker_array()
       // if(red>1)red=0;
   }
  // pub_marker_array(marker_array);
-  ROS_INFO("pub marker array.");
+  //ROS_INFO("pub marker array.");
 
   return marker_array;
 
