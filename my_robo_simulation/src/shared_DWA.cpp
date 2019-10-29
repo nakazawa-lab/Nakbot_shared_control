f14e0f77670ec9503a19c79535bb4eae34ed2e1a
sskitajima <[id+username]@users.noreply.github.com>
2019-10-29 19:21:24 +0900

    pablo free drive

--- my_robo_simulation/src/shared_DWA.cpp
+++ my_robo_simulation/src/shared_DWA.cpp
@@ -405,6 +405,8 @@ int my_robo::cal_J_sharedDWA(double D)
     LOG.push_back(CandVel[index][1]);
     LOG.push_back(sensor.joy_cmd_vel[0]);
     LOG.push_back(sensor.joy_cmd_vel[1]);
+    LOG.push_back(sensor.odom.twist.twist.linear.x);
+    LOG.push_back(sensor.odom.twist.twist.angular.z);
 
     logfile << LOG[0];
     for (int i = 1; i < LOG.size(); i++)
