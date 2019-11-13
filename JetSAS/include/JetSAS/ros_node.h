a598d12fb27922cd444d505d3460dfde6cb09b95
sskitajima <s.kitajima0162@gmail.com>
2019-11-13 17:44:16 +0900

    1113

--- JetSAS/include/JetSAS/ros_node.h
+++ JetSAS/include/JetSAS/ros_node.h
@@ -1,60 +1,190 @@
 #include <math.h>
+#include <cassert>
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
 #include "sensor_msgs/Joy.h"
 
-class JetSAS_Node{
+namespace JetSAS
+{
+struct position
+{
+    double x = 0;
+    double y = 0;
+    double sin_th = 0;
+    double cos_th = 1;
+};
+
+class Lrf
+{
 private:
-    ros::NodeHandle nh;
-
     ros::Publisher lrf_pub;
-    ros::Publisher odom_pub;
-    ros::Publisher joy_pub;
-
     sensor_msgs::LaserScan scan;
-    sensor_msgs::Joy joy;
-    nav_msgs::Odometry odom;
 
-    int old_encoder_right, old_encoder_left;
+    const int urg_data_num = 682; // 実機で得られる点の数
+    const float urg_angle_increment = (2 * M_PI) / 1024.0;
 
-    double x,y,theta,v,w;
-
-    float joy_axes1, joy_axes0;     // axes1は前後方向, 前が正, axes0は左右方向, 左が正
-
-    double old_time, this_loop_time;
-
-    const int urg_data_num =682;        // 実機で得られる点の数
-    const float urg_angle_increment = (2*M_PI)/1024.0;
+    float scan_calib_multiplier;
 
 public:
-    JetSAS_Node(){
+    Lrf()
+    {
         scan.ranges.resize(urg_data_num);
         scan.angle_increment = urg_angle_increment;
-
-        lrf_pub = nh.advertise<sensor_msgs::LaserScan>("/scan",1);
-        odom_pub = nh.advertise<nav_msgs::Odometry>("/odom",1);
-        joy_pub = nh.advertise<sensor_msgs::Joy>("/joy",1);
     }
 
-    void pub_lrf(){
+    void set_publisher(ros::NodeHandle &nh)
+    {
+        lrf_pub = nh.advertise<sensor_msgs::LaserScan>("/scan", 1);
+    };
+
+    void pub_lrf()
+    {
         lrf_pub.publish(scan);
     };
 
-    void pub_odom(){
+    void make_scan_msgs(long *, const int);
+};
+
+class Joy
+{
+private:
+    ros::Publisher joy_pub;
+    sensor_msgs::Joy joy;
+
+    float joy_axes1, joy_axes0; // axes1は前後方向, 前が正, axes0は左右方向, 左が正
+
+    float axes1_multiplier, axes0_multiplier;
+
+public:
+    Joy()
+    {
+    }
+
+    void set_publisher(ros::NodeHandle &nh)
+    {
+        joy_pub = nh.advertise<sensor_msgs::Joy>("/joy", 1);
+    };
+
+    void pub_joy()
+    {
+        joy_pub.publish(joy);
+    };
+
+    // ラジコンの値を人間の指令の(v,w)に変換...はしなくてよい
+    // 並進方向の大きさをjoy_axes1に, 回転方向の大きさをjoy_axes0に, 最大最小が+-1になるように入れてあげてpubすれば良い
+    void make_joy_msgs(int, int);
+};
+
+class Odom
+{
+private:
+    ros::Publisher odom_pub;
+    nav_msgs::Odometry odom;
+
+    int encoder_right, encoder_left;
+    int old_encoder_right, old_encoder_left;
+
+    double encoder_multiplier_vel;
+
+    double v, w;
+
+    double right_v, left_v;
+
+    position now_p, old_p;
+
+    /// ==TODO== ///
+    const double robot_width = 0.3;
+
+    void set_encoder(int e_right, int e_left)
+    {
+        encoder_right = e_right;
+        encoder_left = e_left;
+    };
+
+    void cal_now_vel(const double this_loop_time);
+
+    void cal_pose(double dt);
+
+public:
+    Odom()
+    {
+    }
+
+    void set_publisher(ros::NodeHandle &nh)
+    {
+        odom_pub = nh.advertise<nav_msgs::Odometry>("/odom", 1);
+    }
+
+    void pub_odom()
+    {
         odom_pub.publish(odom);
     };
 
-    void pub_joy(){
-        joy_pub.publish(joy);
+    void make_odom_msgs(int, int, double);
+
+    // エンコーダの値から今のv,wを求める
+    // 昔のx,yの情報と, 今のv,wとこの1ループの時間から新しい位置x,y を求める
+
+    // set_encoder → cal_now_vel → cal_pose
+};
+
+class Cmd_vel
+{
+private:
+    ros::Subscriber sub_vel;
+    geometry_msgs::Twist cmd_vel;
+
+public:
+    Cmd_vel()
+    {
+        cmd_vel.linear.x = 0.0;
+        cmd_vel.angular.z = 0.0;
+    }
+
+    void set_subscriber(ros::NodeHandle &nh)
+    {
+        sub_vel = nh.subscribe("/cmd_vel", 10, &cb_vel, this);
+    }
+
+    void cb_vel(const geometry_msgs::Twist::ConstPtr &msgs)
+    {
+        cmd_vel = *msgs;
+    }
+
+    void cmd_vel_to_encoder();
+};
+} // namespace JetSAS
+
+class JetSAS_Node
+{
+private:
+    ros::NodeHandle nh;
+    double old_time, this_loop_time;
+
+    void pub_sensor()
+    {
+        lrf.pub_lrf();
+        odom.pub_odom();
+        joy.pub_joy();
     };
-    
-    void make_scan_msgs(long*,const int);
 
-    void make_joy_msgs();
+public:
+    JetSAS_Node()
+    {
+        lrf.set_publisher(nh);
+        odom.set_publisher(nh);
+        joy.set_publisher(nh);
+    }
 
-    void make_odom_msgs();
+    JetSAS::Lrf lrf;
+
+    JetSAS::Odom odom;
+
+    JetSAS::Joy joy;
+
+    JetSAS::Cmd_vel cmd_vel;
 
     void controlloop();
 };
--- JetSAS/src/ros_node.cpp
+++ JetSAS/src/ros_node.cpp
@@ -8,7 +8,20 @@
 //     delete[] scan;
 // }
 
-void JetSAS_Node::make_scan_msgs(long* urg_data,const int scan_num){
+double add_theorem_sin(double sin_a, double sin_b, double cos_a, double cos_b)
+{
+  //ROS_INFO("sin_a:%f,sin_b:%f,cos_a:%f,cos_b%f",sin_a,sin_b,cos_a,cos_b);
+  double a = (sin_a * cos_b + cos_a * sin_b);
+  return a;
+}
+
+double add_theorem_cos(double sin_a, double sin_b, double cos_a, double cos_b)
+{
+  double a = (cos_a * cos_b - sin_a * sin_b);
+  return a;
+}
+
+void JetSAS::Lrf::make_scan_msgs(long* urg_data,const int scan_num){
     //std::cout << "in make scan msgs, urg_data num is " << scan_num <<std::endl;
 
     //std::cout << "urg_data[0] " << urg_data[0]/100.0 <<" " << urg_angle_increment <<" "<< scan.angle_increment<<std::endl;
@@ -23,4 +36,73 @@ void JetSAS_Node::make_scan_msgs(long* urg_data,const int scan_num){
         }
         //std::cout << "finish make sensor msgs" <<std::endl;
     }
+}
+
+void JetSAS::Odom::cal_now_vel(const double this_loop_time){
+    assert(this_loop_time != 0);
+
+    right_v = (encoder_right - old_encoder_right) / this_loop_time * encoder_multiplier_vel;
+    left_v = (encoder_left - old_encoder_left) / this_loop_time * encoder_multiplier_vel;
+
+    // ここの符号は実験の結果変わるかもしれない
+    v = (right_v + left_v) / 2;
+    w = (right_v - left_v) / robot_width; 
+
+    old_encoder_right =  encoder_right;
+    old_encoder_left = encoder_left;
+}
+
+void JetSAS::Odom::cal_pose(double dt){
+    double vx = v * old_p.cos_th;
+    double vy = v * old_p.sin_th;
+
+    now_p.x = old_p.x + vx * dt;
+    now_p.y = old_p.y + vy * dt;
+
+    // cos(th+wt),sin(th+wt)を求める
+    now_p.cos_th = add_theorem_cos(old_p.sin_th, sin(w * dt), old_p.cos_th, cos(w * dt));
+    now_p.sin_th = add_theorem_sin(old_p.sin_th, sin(w * dt), old_p.cos_th, cos(w * dt));
+
+    old_p = now_p;
+}
+
+void JetSAS::Odom::make_odom_msgs(int e_right, int e_left, double this_loop_time){
+    set_encoder(e_right,e_left);
+    cal_now_vel(this_loop_time);
+    cal_pose(this_loop_time);
+
+
+    // quaternion(x,y,z,w)に対して,方向ベクトル(lambda_x,lambda_y,lambda_z)に角度theta だけ回転するとき
+    // x = lambda_x * sin(theta/2)
+    // y = lambda_y * sin(theta/2)
+    // z = lambda_z * sin(theta/2)
+    // w = cos(theta/2)
+    double sin_half_theta = sqrt((1-now_p.cos_th)/2);
+    double cos_half_theta = sqrt((1-now_p.cos_th)/2);
+    odom.pose.pose.position.x = now_p.x;
+    odom.pose.pose.position.y = now_p.y;
+
+    odom.pose.pose.orientation.x = sin_half_theta;
+    odom.pose.pose.orientation.y = sin_half_theta;
+    odom.pose.pose.orientation.z = sin_half_theta;
+    odom.pose.pose.orientation.w = cos_half_theta;
+
+    odom.twist.twist.linear.x = v;
+    odom.twist.twist.angular.z = w;
+}
+
+void JetSAS_Node::controlloop(){
+    // urgの値をとってくる　get_urg
+
+    // エンコーダの値をサンプルプログラムからとってくる jetstas e
+
+    // RCの値をサンプルプログラムからとってくる jetsas r
+
+    // odom, lrf, joyをpublish
+    pub_sensor();
+
+    // subscribeした情報をshが理解できる値に変換する
+    cmd_vel.cmd_vel_to_encoder();
+
+    // SHに送信する jetsas v
 }
