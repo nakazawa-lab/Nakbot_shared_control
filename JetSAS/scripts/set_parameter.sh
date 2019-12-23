8faec64487f9a1d22bac1d788eeb118b3b58abde
sskitajima <[id+username]@users.noreply.github.com>
2019-12-24 00:14:39 +0900

    1223

--- JetSAS/scripts/set_parameter.sh
+++ JetSAS/scripts/set_parameter.sh
@@ -1,9 +1,9 @@
 rosparam set /my_robo/diff_drive_controller/angular/z/has_acceleration_limits True
 rosparam set /my_robo/diff_drive_controller/angular/z/has_velocity_limits True
 rosparam set /my_robo/diff_drive_controller/angular/z/max_acceleration 7.01
-rosparam set /my_robo/diff_drive_controller/angular/z/max_velocity 2.12
+rosparam set /my_robo/diff_drive_controller/angular/z/max_velocity 2.03
 rosparam set /my_robo/diff_drive_controller/angular/z/min_acceleration -7.01
-rosparam set /my_robo/diff_drive_controller/angular/z/min_velocity -2.12
+rosparam set /my_robo/diff_drive_controller/angular/z/min_velocity -2.03
 rosparam set /my_robo/diff_drive_controller/cmd_vel_timeout 10.0
 rosparam set /my_robo/diff_drive_controller/linear/x/has_acceleration_limits True
 rosparam set /my_robo/diff_drive_controller/linear/x/has_velocity_limits True
@@ -11,3 +11,5 @@ rosparam set /my_robo/diff_drive_controller/linear/x/max_acceleration 1.0
 rosparam set /my_robo/diff_drive_controller/linear/x/max_velocity 0.31
 rosparam set /my_robo/diff_drive_controller/linear/x/min_acceleration -1.0
 rosparam set /my_robo/diff_drive_controller/linear/x/min_velocity -0.27
+# robot width 0.285
+# (0.31 - (0.27))/2.0 = 2.03
